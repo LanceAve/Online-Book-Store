2cc1495a086668e17ea1891283fed9ab7128302b
LanceAve <lanceave@protonmail.com>
2025-05-14 17:37:06 -0400

    finalized checkoutservice

--- src/main/java/com/lanceave/onlinebookstore/services/CheckoutService.java
+++ src/main/java/com/lanceave/onlinebookstore/services/CheckoutService.java
@@ -1,1 +1,28 @@
 package com.lanceave.onlinebookstore.services;
+
+import com.lanceave.onlinebookstore.entities.BookCartList;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckoutService {
+
+    private final CartService cartService;
+
+    public CheckoutService(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    public double getSubtotal(BookCartList cart) {
+        return cartService.calculateSubtotal(cart);
+    }
+
+    public double getTax(BookCartList cart) {
+        double subtotal = cartService.calculateSubtotal(cart);
+        return cartService.calculateTax(subtotal);
+    }
+
+    public double getTotal(BookCartList cart) {
+        double subtotal = cartService.calculateSubtotal(cart);
+        return cartService.calculateTotal(subtotal);
+    }
+}

3a19e8a13d40486df08f55aa11c55283785039ae
LanceAve <lanceave@protonmail.com>
2025-05-14 16:55:14 -0400

    initial structure of cartservice

--- src/main/java/com/lanceave/onlinebookstore/services/CartService.java
+++ src/main/java/com/lanceave/onlinebookstore/services/CartService.java
@@ -1,1 +1,11 @@
 package com.lanceave.onlinebookstore.services;
+
+import com.lanceave.onlinebookstore.entities.Book;
+import com.lanceave.onlinebookstore.entities.BookCartList;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CartService {
+

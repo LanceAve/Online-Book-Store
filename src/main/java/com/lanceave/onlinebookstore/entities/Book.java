4fb055b08e0bb775e7fa7a34e79acca019e01081
LanceAve <lanceave@protonmail.com>
2025-05-14 16:48:41 -0400

    finalized book class

--- src/main/java/com/lanceave/onlinebookstore/entities/Book.java
+++ src/main/java/com/lanceave/onlinebookstore/entities/Book.java
@@ -1,1 +1,21 @@
 package com.lanceave.onlinebookstore.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Random;
+
+@Getter
+@Setter
+public class Book {
+    private String bookISBN;
+    private String bookTitle;
+    private String bookAuthor;
+    private double bookPrice;
+
+    public Book(String bookTitle, String bookAuthor, double bookPrice) {
+        this.bookISBN = generateBookISBN();
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookPrice = bookPrice;
+    }
+

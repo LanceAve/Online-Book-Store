d02fdd22286f5d839b9778c273010ced94a050fa
LanceAve <lanceave@protonmail.com>
2025-05-14 16:49:23 -0400

    finalized bookcartlist

--- src/main/java/com/lanceave/onlinebookstore/entities/BookCartList.java
+++ src/main/java/com/lanceave/onlinebookstore/entities/BookCartList.java
@@ -1,1 +1,33 @@
 package com.lanceave.onlinebookstore.entities;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Component
+public class BookCartList {
+    private final List<Book> cartBooks = new ArrayList<>();
+
+    public void addToCart(Book book) {
+        cartBooks.add(book);
+    }
+
+    public double getSubtotal() {
+        return cartBooks.stream().mapToDouble(Book::getBookPrice).sum();
+    }
+
+    public double getSalesTax(double taxRate) {
+        return getSubtotal() * taxRate;
+    }
+
+    public double getTotal(double taxRate) {
+        return getSubtotal() + getSalesTax(taxRate);
+    }
+
+    public int getCartSize() {
+        return cartBooks.size();
+    }
+}

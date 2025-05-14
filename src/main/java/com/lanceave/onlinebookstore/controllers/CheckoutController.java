965b471d3a4a2d1df61e73e4d4098bfb86b55c2f
LanceAve <lanceave@protonmail.com>
2025-05-14 16:53:12 -0400

    initial /checkout endpoint

--- src/main/java/com/lanceave/onlinebookstore/controllers/CheckoutController.java
+++ src/main/java/com/lanceave/onlinebookstore/controllers/CheckoutController.java
@@ -1,1 +1,22 @@
 package com.lanceave.onlinebookstore.controllers;
+
+import com.lanceave.onlinebookstore.entities.BookCartList;
+import com.lanceave.onlinebookstore.services.CheckoutService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DecimalFormat;
+
+@Controller
+@RequestMapping("/checkout")
+public class CheckoutController {
+
+    private final CheckoutService checkoutService;
+    private final DecimalFormat df = new DecimalFormat("#0.00");
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+

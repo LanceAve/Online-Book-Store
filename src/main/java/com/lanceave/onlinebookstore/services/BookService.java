eb90a2a76fdceda79bc3caffd2ffb873c919da00
LanceAve <lanceave@protonmail.com>
2025-05-14 16:53:55 -0400

    initial structure for bookservice

--- src/main/java/com/lanceave/onlinebookstore/services/BookService.java
+++ src/main/java/com/lanceave/onlinebookstore/services/BookService.java
@@ -1,1 +1,10 @@
 package com.lanceave.onlinebookstore.services;
+
+import com.lanceave.onlinebookstore.entities.Book;
+import com.lanceave.onlinebookstore.entities.BookList;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {

78f4d638b470478af49e1359a76182b58e169c07
LanceAve <lanceave@protonmail.com>
2025-05-14 16:50:50 -0400

    initial /book map implemented

--- src/main/java/com/lanceave/onlinebookstore/controllers/BookController.java
+++ src/main/java/com/lanceave/onlinebookstore/controllers/BookController.java
@@ -1,1 +1,28 @@
 package com.lanceave.onlinebookstore.controllers;
+
+import com.lanceave.onlinebookstore.entities.Book;
+import com.lanceave.onlinebookstore.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    // Available Books Page
+    @GetMapping
+    public String showAvailableBooks(Model model) {
+        model.addAttribute("books", bookService.getAllBooks());
+        model.addAttribute("newBook", new Book("", "", 0.0)); // empty book for the form
+        return "available_books";
+    }
+

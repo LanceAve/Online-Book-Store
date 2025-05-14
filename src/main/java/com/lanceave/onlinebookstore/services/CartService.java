ab6c0f12cc36ef4431aa359e2c32cac6bb81e582
LanceAve <lanceave@protonmail.com>
2025-05-14 16:56:06 -0400

    defined methods for cart service

--- src/main/java/com/lanceave/onlinebookstore/services/CartService.java
+++ src/main/java/com/lanceave/onlinebookstore/services/CartService.java
@@ -9,3 +9,29 @@ import java.util.List;
 @Service
 public class CartService {
 
+    public void addBookToCart(BookCartList cart, Book book) {
+        cart.addToCart(book);
+    }
+
+    public List<Book> getCartItems(BookCartList cart) {
+        return cart.getCartBooks();
+    }
+
+    public int getItemCount(BookCartList cart) {
+        return cart.getCartSize();
+    }
+
+    public double calculateSubtotal(BookCartList cart) {
+        return cart.getCartBooks().stream()
+                .mapToDouble(Book::getBookPrice)
+                .sum();
+    }
+
+    public double calculateTax(double subtotal) {
+        return subtotal * 0.13;
+    }
+
+    public double calculateTotal(double subtotal) {
+        return subtotal + calculateTax(subtotal);
+    }
+}

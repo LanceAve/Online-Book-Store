48f5a9fc1f10325b198b616dc2a671f13ec88cc5
LanceAve <lanceave@protonmail.com>
2025-05-14 16:51:57 -0400

    initial /cart enpoint defined

--- src/main/java/com/lanceave/onlinebookstore/controllers/CartController.java
+++ src/main/java/com/lanceave/onlinebookstore/controllers/CartController.java
@@ -1,1 +1,22 @@
 package com.lanceave.onlinebookstore.controllers;
+
+import com.lanceave.onlinebookstore.entities.Book;
+import com.lanceave.onlinebookstore.entities.BookCartList;
+import com.lanceave.onlinebookstore.services.BookService;
+import com.lanceave.onlinebookstore.services.CartService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    private final BookService bookService;
+    private final CartService cartService;
+
+    public CartController(BookService bookService, CartService cartService) {
+        this.bookService = bookService;
+        this.cartService = cartService;
+    }
+
